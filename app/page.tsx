113db68c733f1533377849a13604862e03ef2d95
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-21 14:30:28 +0800

    custom component next js

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Link from 'next/link';
+import Header from '../components/Header/header';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/users">User</Link>
-          </li>
-        </ul>
-      </header>
+     <Header></Header>
     </main>
   );
 }
