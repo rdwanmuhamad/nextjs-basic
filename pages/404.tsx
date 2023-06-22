4500981d979fb45ecfeac8016d40ca9a3a9a7626
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-22 08:28:39 +0800

    auto redirection nextjs

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,13 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 export default function Custom404() {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 2000)
+    })
   return (
     <><h1>Oooppppssss....</h1>
     <h1>Your page not found...</h1></>
