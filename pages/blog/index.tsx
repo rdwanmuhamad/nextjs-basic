5c493bd30b896bf869751e5416ff95cc8dadf4f0
rdwanmuhamad <mrdwn624@gmail.com>
2023-06-22 12:26:50 +0800

    Data Fetching getServerSideProps

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,40 @@
 import Layout from '@/components/Layout'
 import React from 'react'
+import styles from './blog.module.css'
 
-export default function index() {
+
+interface Post{
+  id: number,
+  title: string,
+  body: string
+}
+interface DataBlogProps {
+  dataBlog: Post[];
+}
+
+export default function Blog(props: DataBlogProps) {
+  const {dataBlog} = props;
   return (
     <Layout pageTitle="Blog Page">
       <div>Blog page</div>
+      {dataBlog.map((blog) => (
+        <div key={blog.id} className={styles.card}>
+          <p>{blog.title}</p>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const dataBlog = await res.json();
+
+  return{
+    props: {
+      dataBlog
+    }
+  }
+  
 }
